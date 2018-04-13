4d8eaece308f32b75ecc15f411ef2c98968f0691
Kanchan Thareja <kthareja12@gmail.com>
2018-04-12 17:09:57 -0700

    routing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,30 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+import { trigger,style,transition,animate,query,keyframes,stagger } from '@angular/animations';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   animations: [
-  trigger('goals', [
-    transition('* => *', [
-      query(':enter', style({ opacity: 0 }), {optional: true}),
-
-      query(':enter', stagger('300ms', [
-        animate('.6 ease-in', keyframes([
-          style({opacity:0, transform: 'translateY(-75%)', offset:0}),
-          style({opacity:.5, transform: 'translateY(35px)', offset:.3}),
-          style({opacity:1, transform: 'translateY(0)', offset:1})
-        ]))]), {optional:true}),
-
-        query(':leave', stagger('300ms', [
-          animate('.6 ease-in', keyframes([
-            style({opacity:1, transform: 'translateY(0)', offset:0}),
-            style({opacity:.5, transform: 'translateY(35px)', offset:.3}),
-            style({opacity:0, transform: 'translateY(-75%)', offset:1}),
-          ]))]), {optional:true}),
-
+    trigger('goals',[
+      transition('* => *',[
+        query(':enter', style({opacity: 0}), {optional: true}),
+        query(':enter', stagger('300ms',[
+          animate('.6s ease-in',keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+          ]))
+        ]), {optional: true}),
+      ])
     ])
-  ])
   ]
 })
 export class HomeComponent implements OnInit {
@@ -37,12 +29,12 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
       this.itemCount = this.goals.length;
   }
-addItem(){
+  addItem(){
   this.goals.push(this.goalText);
   this.goalText = '';
   this.itemCount = this.goals.length;
 }
-removeItem(i){
+  removeItem(i){
   this.goals.splice(i, 1);
 }
 }
